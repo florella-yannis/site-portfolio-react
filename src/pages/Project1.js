daa7f1923bea03507dfc69bbeaa896e590750cdd
florella-yannis <florellayannis@gmail.com>
2024-02-18 15:12:36 +0100

    pages

---
+++ src/pages/Project1.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Project1 = () => {
+    return (
+        <h1>Project1</h1>
+    );
+};
+
+export default Project1;
---
+++ src/pages/Project2.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Project2 = () => {
+    return (
+        <h1>Project2</h1>
+    );
+};
+
+export default Project2;
