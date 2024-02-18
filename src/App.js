1ad58e467cca6efcc759926aa1dd91cf62b9c96c
florella-yannis <florellayannis@gmail.com>
2024-02-18 15:33:11 +0100

    init + 404error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Project from './pages/Project';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
-    <h1>Hello</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projet/:id" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
